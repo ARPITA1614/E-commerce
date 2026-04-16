b401aa376bdaf84cae28847ce847c9da4d90047f
ARPITA1614 <arpitadmn.com>
2026-04-16 15:55:47 +0530

    searh bar

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,8 +5,8 @@ class Product < ApplicationRecord
     has_many :wishlists
     has_many :wishlisted_by, through: :wishlists, source: :user
 
-      def self.ransackable_attributes(auth_object = nil)
-    ["category_id", "created_at", "description", "id", "id_value", "name", "price", "updated_at"]
-  end
+    def self.ransackable_attributes(auth_object = nil)
+     ["category_id", "created_at", "description", "id", "id_value", "name", "price", "updated_at"]
+    end
 
 end
